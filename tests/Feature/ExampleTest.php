4c1e2e3f88e958eb0d8f76a644823735e4d9bbe2
Condurache Maria <mariacondurach@gmail.com>
2026-06-24 12:04:06 +0300

    tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,6 +13,7 @@ class ExampleTest extends TestCase
 
     public function test_game_and_rounds_are_created()
     {
+        $this->withoutExceptionHandling(); // <--- Add this temporarily
         // 1. Executăm acțiunea (pornirea jocului)
         $response = $this->get('/start-game');
 
